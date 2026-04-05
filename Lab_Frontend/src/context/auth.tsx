0bea81f2af2e94cdc1ea69f05a65f6c9572d57ba
kiran04-code <kr551344@gmail.com>
2026-04-05 22:48:05 +0530

    flex:Login_bug

--- Lab_Frontend/src/context/auth.tsx
+++ Lab_Frontend/src/context/auth.tsx
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [success, setSuccess] = useState(false);
   const axioseInstance = axios.create({
-    baseURL:"https://bkdoflab.onrender.com"
+    baseURL:"http://localhost:3005"
   })
   // Fetch user data from backend
   const fetchUser = async () => {
